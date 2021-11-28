e3cc68dde8197cde5d1e5b4a9aa03543b52c468c
Nicole Ashlyn Karas <nkaras@uwo.ca>
2021-11-28 15:07:37 -0500

    add comments to other main files

--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
+/***************************************************************************//**
+ * @brief Main function
+ *
+ * Main function responsible for starting the C++ application and window
+ * 
+ * @param argc Arguement number
+ * @param argv Array of arguement characters
+ * @authors Nicole Karas
+ ******************************************************************************/
 #include "mainwindow.h"
 #include <QApplication>
  
